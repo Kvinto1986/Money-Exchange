c97c3d9512a0d0efcceaba27cf72c5840df0c92f
Kvinto1986 <kvinto1986@gmail.com>
2018-09-24 21:07:33 +0300

    update index.js to ES6

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    var exchange={};
-    var coins=["H","Q","D","N","P"];
-    var change=[50,25,10,5,1];
-    var money=[change.length];
-    var count=currency;
-    for(var i=0;i<change.length;i++){
-        if(count==0)
-            money[i]=0;
-        money[i]=Math.floor(count/change[i]);
-        count=count-change[i]*money[i];
+    let exchange = {};
+    const COINS = ["H", "Q", "D", "N", "P"];
+    const CHANGE = [50, 25, 10, 5, 1];
+    let money = [CHANGE.length];
+    let count = currency;
+    for (let i = 0; i < CHANGE.length; i++) {
+        if (count === 0)
+            money[i] = 0;
+        money[i] = Math.floor(count / CHANGE[i]);
+        count = count - CHANGE[i] * money[i];
     }
 
 
-    for (var i=0;i<money.length;i++) {
-        if(money[i]>0)
-            exchange[coins[i]]=money[i];
+    for (let i = 0; i < money.length; i++) {
+        if (money[i] > 0)
+            exchange[COINS[i]] = money[i];
     }
 
-    var too_rich={
-        error:"You are rich, my friend! We don't have so much coins for exchange"
+    const TOO_RICH = {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
     }
 
-    if(currency>10000){
-        return too_rich;
-    }
+    if (currency > 10000)
+        return TOO_RICH;
 
     else return exchange;
 }
